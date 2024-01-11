6d2365bc5376c7ee42b06260a003331fc125945d
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-01-11 14:06:25 -0400

    sign-in and sign-out methods

--- app/src/main/java/com/example/whatsaapcloneapp/viewmodel/MyViewModel.java
+++ app/src/main/java/com/example/whatsaapcloneapp/viewmodel/MyViewModel.java
@@ -1,2 +1,32 @@
-package com.example.whatsaapcloneapp.viewmodel;public class MyViewModel {
+package com.example.whatsaapcloneapp.viewmodel;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.whatsaapcloneapp.Repository.Repository;
+
+public class MyViewModel extends AndroidViewModel {
+
+    Repository repository;
+    public MyViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository();
+    }
+
+    //Auth
+    public void signUpAnonymousUser(){
+        Context c = this.getApplication();
+        repository.firebaseAnonymousAuth(c);
+    }
+
+    public String getCurrentUserId(){
+        return repository.getCurrentUserId();
+    }
+
+    public void signOut(){
+        repository.signOUT();
+    }
 }
