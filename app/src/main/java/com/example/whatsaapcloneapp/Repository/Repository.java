31abb68bc3e36f5b4c99da5fa21dba225044cb07
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-01-11 13:48:51 -0400

    added anonymous authentication

---
+++ app/src/main/java/com/example/whatsaapcloneapp/Repository/Repository.java
@@ -0,0 +1,30 @@
+package com.example.whatsaapcloneapp.Repository;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.example.whatsaapcloneapp.views.GroupsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Repository {
+
+    public void firebaseAnonymousAuth(Context context){
+        FirebaseAuth.getInstance().signInAnonymously()
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent i = new Intent(context, GroupsActivity.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(i);
+                        }
+                    }
+                });
+    }
+}
