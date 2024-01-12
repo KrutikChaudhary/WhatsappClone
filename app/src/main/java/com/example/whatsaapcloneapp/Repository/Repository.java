28c9f280d24682c76f8a726816ae121b54952f5b
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-01-12 08:51:10 -0400

    functionality to receive groups from firebase

--- app/src/main/java/com/example/whatsaapcloneapp/Repository/Repository.java
+++ app/src/main/java/com/example/whatsaapcloneapp/Repository/Repository.java
@@ -4,16 +4,38 @@ import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 
+import com.example.whatsaapcloneapp.model.ChatGroup;
 import com.example.whatsaapcloneapp.views.GroupsActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Repository {
 
+    MutableLiveData<List<ChatGroup>> chatGroupMutableLiveData;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    public Repository() {
+        chatGroupMutableLiveData = new MutableLiveData<>();
+        this.database = FirebaseDatabase.getInstance();
+        this.reference = database.getReference();
+    }
+
+    //Authentication
     public void firebaseAnonymousAuth(Context context){
         FirebaseAuth.getInstance().signInAnonymously()
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -36,5 +58,28 @@ public class Repository {
     //SignOut Functionality
     public void signOUT(){
         FirebaseAuth.getInstance().signOut();
+    }
+
+    public MutableLiveData<List<ChatGroup>> getChatGroupMutableLiveData() {
+        List<ChatGroup> groupList = new ArrayList<>();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                groupList.clear();
+
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    ChatGroup group = new ChatGroup(dataSnapshot.getKey());
+                    groupList.add(group);
+                }
+
+                chatGroupMutableLiveData.postValue(groupList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return chatGroupMutableLiveData;
     }
 }
