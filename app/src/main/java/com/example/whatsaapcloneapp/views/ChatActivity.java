0b9958e75a63865cc8af43abdae07f0e15d7d1ac
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-01-13 10:33:45 -0400

    chat activity fuctionality

--- app/src/main/java/com/example/whatsaapcloneapp/views/ChatActivity.java
+++ app/src/main/java/com/example/whatsaapcloneapp/views/ChatActivity.java
@@ -1,16 +1,59 @@
 package com.example.whatsaapcloneapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.whatsaapcloneapp.R;
+import com.example.whatsaapcloneapp.databinding.ActivityChatBinding;
+import com.example.whatsaapcloneapp.model.ChatMessage;
+import com.example.whatsaapcloneapp.viewmodel.MyViewModel;
+import com.example.whatsaapcloneapp.views.adapters.ChatAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+    private ActivityChatBinding binding;
+    private MyViewModel myViewModel;
+    private RecyclerView recyclerView;
+    private ChatAdapter myAdapter;
+    private List<ChatMessage> messageList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_chat);
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        String groupName = getIntent().getStringExtra("GROUP_NAME");
+        myViewModel.getMessagesLiveData(groupName).observe(this, new Observer<List<ChatMessage>>() {
+            @Override
+            public void onChanged(List<ChatMessage> chatMessages) {
+                messageList = new ArrayList<>();
+                messageList.addAll(chatMessages);
+
+                myAdapter = new ChatAdapter(messageList,getApplicationContext());
+                recyclerView.setAdapter(myAdapter);
+                myAdapter.notifyDataSetChanged();
+
+                int latestPosition = myAdapter.getItemCount() -1;
+                recyclerView.smoothScrollToPosition(latestPosition);
+            }
+        });
+
+        binding.setVModel(myViewModel);
     }
 }
